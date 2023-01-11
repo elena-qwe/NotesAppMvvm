4c2f61fa546fe9f92df9b5f0b1a2757227b0479c
hxney <malcevalena301@gmail.com>
2023-01-11 19:55:30 +0300

    1

---
+++ app/src/main/java/com/example/notesappmvvm/screens/Start.kt
@@ -0,0 +1,70 @@
+package com.example.notesappmvvm.screens
+
+import android.app.Application
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.notesappmvvm.MainViewModel
+import com.example.notesappmvvm.MainViewModelFactory
+import com.example.notesappmvvm.navigation.NavRoute
+import com.example.notesappmvvm.ui.theme.NotesAppMVVMTheme
+import com.example.notesappmvvm.utils.TYPE_FIREBASE
+import com.example.notesappmvvm.utils.TYPE_ROOM
+
+@Composable
+fun StartScreen(navController: NavHostController) {
+    val context = LocalContext.current
+    val mViewModel: MainViewModel =
+        viewModel(factory = MainViewModelFactory(context.applicationContext as Application))
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "What will we use?")
+            Button(
+                onClick = {
+                    mViewModel.initDatabase(TYPE_ROOM)
+                    navController.navigate(route = NavRoute.Main.route)
+                },
+                modifier = Modifier
+                    .width(200.dp)
+                    .padding(vertical = 8.dp)
+            ) {
+                Text(text = "Room database")
+            }
+            Button(
+                onClick = {
+                    mViewModel.initDatabase(TYPE_FIREBASE)
+                    navController.navigate(route = NavRoute.Main.route)
+                },
+                modifier = Modifier
+                    .width(200.dp)
+                    .padding(vertical = 8.dp)
+            ) {
+                Text(text = "Firebase database")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun prevStartScreen() {
+    NotesAppMVVMTheme {
+        StartScreen(navController = rememberNavController())
+    }
+}
